c91a85cb9a6e1dc30e6e958db92d2774d48f04a3
somiframe <yj.somi@gmail.com>
2017-05-31 16:29:52 +0700

    update

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import MSE from "../src/MediaSourceE"
-var mse = new  MSE('#vid1', '/video/VivaLaVida_dashinit.mp4',
-    'video/mp4;codecs="avc1.4D401F,mp4a.40.2""',
+var mse = new  MSE('#vid1', '/dashStream',
+    'video/mp4;codecs="avc1.4D401F,mp4a.40.2"',
     {start: 0, end: 1436},
     {start: 1437, end: 1900}
 );
+console.log(mse);
